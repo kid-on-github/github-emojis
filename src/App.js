2e69b9f3965d3c22cb3a92cda9358920ca3323d2
Mike Peterson <mike@lemonshell.com>
2021-05-03 12:20:08 -0700

    Basic emoji search with topbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [emojis, setEmojis] = useState({})
+  const [search, setSearch] = useState('')
+  const [emoji, setEmoji] = useState('1f999')
+
+  useEffect(
+    () => {
+      if (Object.keys(emojis).length === 0){
+        fetch('https://api.github.com/emojis')
+        .then(r => r.json())
+        .then(r => setEmojis(r))
+      }
+    }
+  )
+
+
+    console.log(search)
+    
+    
+  const searchTest = new RegExp(search, 'g')
+
+  const icons = Object.keys(emojis).map(
+    label => {
+
+      if (search.length > 0){
+        let isInSearch = searchTest.test(label)
+        if (!isInSearch){return ''}
+      }      
+
+
+      return (
+        <div className='emoji' key={label} onClick={
+          () => {
+          
+          const fileName = emojis[label].match(/(\d|\w)+.png/g)
+          const code = fileName.toString().slice(0, fileName.length - 5)
+          setEmoji(code)
+        
+          }
+        }>
+          
+          <img src={emojis[label]} alt={label}/>
+        </div>
+      )
+    }
+  )
+
+
+  //(e) => setSearch(e.target.value)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='topbar'>
+
+
+        <div className='search'>
+          <input type='text' onChange={(e) => setSearch(e.target.value.toLowerCase())} placeholder='search'/>
+        </div>
+
+      </div>
+
+      <div className='emojiWrapper'>
+        {icons}
+      </div>
     </div>
   );
 }
